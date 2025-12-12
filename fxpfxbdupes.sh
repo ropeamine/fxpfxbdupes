ccd1e6b32d618ef53446ea23af589a03621a8913
ropeamine <ropeaminemusic@gmail.com>
2025-12-12 01:10:23 -0600

    Create fxpfxbdupes.sh

---
+++ fxpfxbdupes.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+DUP=~/Desktop/Duplicates_$(date +%Y%m%d_%H%M%S)
+LOG=~/Desktop/Duplicates_$(date +%Y%m%d_%H%M%S).log
+mkdir -p "$DUP"
+
+{ DIR=~/Library/Application\ Support/LennarDigital/Sylenth1/SoundBanks
+typeset -A f; t=0; d=0
+for x in "$DIR"/**/*(.N); do
+  [[ "$x" == *.fxb || "$x" == *.fxp ]] || continue
+  ((t++))
+  c=$(md5 -q "$x")
+  rel=${x#"$DIR"/}
+  echo "Processing: $rel md5 $c"
+  f[$c]+="$x"$'\n'
+done
+for c l in ${(kv)f}; do
+  (( $(echo "$l" | wc -l) > 1 )) || continue
+  first=true
+  while read -r y; do
+    [[ -z "$y" ]] && continue
+    rel=${y#"$DIR"/}
+    if $first; then first=false
+    else ((d++)); echo "Duplicate Found: $rel moving to $DUP"; mv "$y" "$DUP/"; fi
+  done <<< "$l"
+done
+echo "Processed $t files, moved $d duplicates."
+} | tee "$LOG"
